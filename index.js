2c2c2960e2007ef1ecc94154cab87ad0cb7c9902
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-06-22 15:18:01 +0600

    vercel

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
                   res.json(result);
             });
 
-            await client.db("admin").command({ ping: 1 });
+            // await client.db("admin").command({ ping: 1 });
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
       } finally {
             // Ensures that the client will close when you finish/error
